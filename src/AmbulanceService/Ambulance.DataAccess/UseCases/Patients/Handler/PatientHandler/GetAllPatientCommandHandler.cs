e6d366e6380256e0ae683437bcd6887696677a7f
farxodbek Madrahimov <farhodbek914@gmail.com>
2023-12-07 13:56:18 +0500

    patient post and get

---
+++ src/AmbulanceService/Ambulance.DataAccess/UseCases/Patients/Handler/PatientHandler/GetAllPatientCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Ambulance.Application.Absreactions;
+using Ambulance.Application.UseCases.Doctors.Queries.DoctorQueries;
+using Ambulance.Application.UseCases.Patients.Queries.PatientQueries;
+using Ambulance.Domain.Entitites.Doctors;
+using Ambulance.Domain.Entitites.Patients;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambulance.Application.UseCases.Patients.Handler.PatientHandler
+{
+    public class GetPatientCommandHandler : IRequestHandler<GetAllPatientsQuery, IEnumerable<Patient>>
+    {
+        private readonly IAmbulanceDbContext _context;
+
+        public GetPatientCommandHandler(IAmbulanceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Patient>> Handle(GetAllPatientsQuery request, CancellationToken cancellationToken)
+        {
+            List<Patient> result = await _context.patients.ToListAsync(cancellationToken);
+            if (result == null)
+                throw new Exception();
+            return result;
+        }
+    }
+}
--- src/AmbulanceService/Ambulance.Domain/Dtos/PatientDtos/PatientDto.cs
+++ src/AmbulanceService/Ambulance.Domain/Dtos/PatientDtos/PatientDto.cs
@@ -9,8 +9,6 @@ namespace Ambulance.Domain.Dtos.PatientDtos
 {
     public class PatientDto
     {
-        public int Id { get; set; }
-
         
         public string Name { get; set; }
 
