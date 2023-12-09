edd0f26c194525d612b1687dec90cebc2563b67a
farxodbek Madrahimov <farhodbek914@gmail.com>
2023-12-09 11:28:56 +0500

    co blya

--- src/APIGetAway/APIGetAway/Program.cs
+++ src/APIGetAway/APIGetAway/Program.cs
@@ -1,5 +1,5 @@
 using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,10 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
